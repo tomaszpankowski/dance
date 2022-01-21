309f7dd56da7d2a053bdcb6b73266ac20885c06f
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 13:07:55 +0100

    modified routing

---
+++ src/views/dance.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Dance extends Component{
+    render(){
+        return(        
+            <Container fluid className="dance-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Dance;
