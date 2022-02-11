00a1716614049715797f4ee05fb8c59fb881ec96
tomaszpankowski <pankowski.email@gmail.com>
2022-02-11 01:31:41 +0100

    layout done

--- src/views/dance.js
+++ src/views/dance.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import DanceS1 from "../components/dance-s1";
+import DanceS2 from "../components/dance-s2";
+import DanceS3 from "../components/dance-s3";
 
 class Dance extends Component{
     render(){
         return(        
-            <Container fluid className="dance-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <DanceS1/>
+                <DanceS2/>
+                <DanceS3/>
             </Container>    
         );
     }
